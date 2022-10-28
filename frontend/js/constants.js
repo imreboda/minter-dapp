9cf86bc2c48572b0789fde0da73545ef996d6bd3
imreboda <imreboda@gmail.com>
2022-10-28 11:59:51 +0200

    site under construction

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the worldchips NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = "Collection of world's great sights in famous painters' style";
+const welcome_p = "Site under construction";
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
